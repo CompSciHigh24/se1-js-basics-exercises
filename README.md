64da0286158e8b1faa026ba4b9deb2b9774bc3f4
CompSciHigh24 <csh-github@compscihigh.org>
2024-06-18 19:04:27 +0000

    Lab template

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# se1-js-basics-exercises
-Covers console log and variables
+# JavaScript Basics (Variables & Console Logging)
+## Instructions
+Work in pairs to complete the tasks as specified by the comments. Remember to follow driver-navigator model for group work, which means only one of you needs to complete the Repl.it.
+
+*Roles*:
+- Navigator: References notes/documentation to gives instructions. Usually is more comfortable with the material.
+- Driver: Only person with tech out and is coding instructions.
---
+++ index.js
@@ -0,0 +1,53 @@
+// VARIABLES
+// For the navigator (person giving instructions),
+// Define three variables: `name`, `numberOfSiblings`, and `isBilingual`. Determine which should be constants and which may change.
+// Assign corresponding values with the most appropriate types for each
+let name = "Ms Pham"
+let numberOfSiblings = 1
+let isBilingual = true
+
+// Print these variables to the console.
+console.log(name)
+console.log(numberOfSiblings)
+console.log(isBilingual)
+
+// RE-ASSIGNMENT
+// Create a variable `counter` and initialize it with 0.
+let counter = 0
+// Set it to a larger value
+counter = 9
+// Increment the `counter` by 1 without specifying the number!
+counter = counter + 1
+counter++
+counter += 1
+// Print the updated value of `counter`.
+console.log(counter)
+
+// DATA TYPES
+// Create variables for various primitive data types: `number`, `string`, `boolean`, and `null`.
+let number = 9
+let string = "hello"
+let boolean = true
+let Null = null
+
+// Print the values and confirm their type using typeof
+console.log(typeof boolean)
+
+// Create a variable that has an undefined type 
+
+// Print the variable and confirm its type
+
+
+
+// EXTRA CREDIT (Google these!)
+// Make an array of siblings/languages that you speak
+
+
+
+// Make an object with your name, siblings, and languages
+
+
+
+// Try printing the number of siblings you have using ONLY the object (Hint: look up how to find the length of an array)
+
+
